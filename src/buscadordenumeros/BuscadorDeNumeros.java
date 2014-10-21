86c4e1d3eca73b1efd60ca32e59c358779fbf23e
yoseef <yoseef@gmail.com>
2014-10-22 00:16:41 +0200

    Desenvolpuat el programa!

---
+++ src/buscadordenumeros/BuscadorDeNumeros.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buscadordenumeros;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Timer;
+
+/**
+ *
+ * @author user
+ */
+public class BuscadorDeNumeros {
+
+    //Timer timer = new Timer();
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Timer timer = new Timer();
+        //s'ha de crear un llista amb numeros ordenats 
+        Scanner lc = new Scanner(System.in);
+
+        ArrayList<Integer> llista = new ArrayList<>();
+        for (int i = 0; i <= 500000; i++) {
+            llista.add(i);
+            //System.out.println(i);
+        }
+        System.out.println("Introdueix numero a buscar: ");
+        int num = lc.nextInt();
+        long start = System.currentTimeMillis();
+        if (llista.contains(num)) {
+            //counter.countPrimes(1000000);
+            long end = System.currentTimeMillis();
+            System.out.println("Trobat en : " + ((end - start)) + "milliseconds");
+        }else{
+            System.out.println("No s'ha trobat el numero.");
+        }
+
+    }
+}
